a852680a7a6b7713044402ce21807a08e19424c0
BluRRayS <45588540+BluRRayS@users.noreply.github.com>
2019-02-19 10:22:05 +0100

    Auto Generated ASP.NET CORE stuff

---
+++ iSpendApp/iSpendApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace iSpendApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
