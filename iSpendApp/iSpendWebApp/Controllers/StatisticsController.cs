7de9ccce8adafb76c3b7266cc43ba4e69d624ff4
Joep Verhoeven <412182@student.fontys.nl>
2019-05-06 21:05:15 +0200

    Added: controller for Statistics and tested chart.js with mutliple charts

---
+++ iSpendApp/iSpendWebApp/Controllers/StatisticsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using iSpendWebApp.Controllers.ActionFilters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iSpendWebApp.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        [HttpGet]
+        [ServiceFilter(typeof(AuthorizationActionFilter))]
+        public IActionResult Index()
+        {
+            var categories = new List<string>();
+            categories.Add("Groceries");
+            categories.Add("health");
+            categories.Add("Taxes");
+            categories.Add("Rent");
+            categories.Add("Fuel");
+            categories.Add("Clothes");
+
+            var numbers = new List<int>();
+            for (int i = 0; i < categories.Count; i++)
+            {
+                numbers.Add(new Random().Next(1,20));
+            }
+
+
+
+            ViewBag.Categories = Newtonsoft.Json.JsonConvert.SerializeObject(categories); 
+            ViewBag.CategoriesCosts = Newtonsoft.Json.JsonConvert.SerializeObject(numbers);
+
+            return View("~/Views/Statistics/Dashboard.cshtml");
+        }
+    }
+}
---
+++ iSpendApp/iSpendWebApp/Models/Statistics/StatisticsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iSpendWebApp.Models.Statistics
+{
+    public class StatisticsViewModel
+    {
+    }
+}
